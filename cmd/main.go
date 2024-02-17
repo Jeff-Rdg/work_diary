ee7b051560c6c57990cf052b63f08dbe4d4de869
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-02-17 14:20:14 -0300

    feat: inject config in project

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"work_diary/config"
+)
+
 func main() {
+	conf, err := config.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = config.LoadDatabase(conf)
+	if err != nil {
+		panic(err)
+	}
 
 }
