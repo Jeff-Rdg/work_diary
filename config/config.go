38cafa39dcb9d9b518e2d0755b6901f1fb9fe8ea
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-02-17 14:18:55 -0300

    feat: create infra to project

---
+++ config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type conf struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+}
+
+func LoadConfig(path string) (*conf, error) {
+	var cfg *conf
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg, nil
+}
+
+func LoadDatabase(conf *conf) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.AutoMigrate()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
